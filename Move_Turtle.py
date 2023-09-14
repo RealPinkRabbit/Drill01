482d170326c1e0af56bbe309c925719c910157b9
RealPinkRabbit <sdg0896@gmail.com>
2023-09-14 22:54:27 +0900

    Add_Escape

--- Move_Turtle.py
+++ Move_Turtle.py
@@ -20,11 +20,15 @@ def go_left():
     turtle.stamp()
     turtle.forward(50)
 
+def reset():
+    turtle.reset()
+
 turtle.shape('turtle')
 
 turtle.onkey(go_up, 'w')
 turtle.onkey(go_right, 'd')
 turtle.onkey(go_down, 's')
 turtle.onkey(go_left, 'a')
+turtle.onkey(reset, 'Escape')
 
 turtle.listen()
